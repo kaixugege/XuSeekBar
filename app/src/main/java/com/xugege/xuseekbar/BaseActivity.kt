affe7d0df9baea12582bbd5a3ae6b73bfdc4c2b7
kaixugege <604312074@qq.com>
2019-08-26 17:36:59 +0800

    Initial commit

---
+++ app/src/main/java/com/xugege/xuseekbar/BaseActivity.kt
@@ -0,0 +1,13 @@
+package com.xugege.xuseekbar
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ *   @Author:       KaixuGege
+ *   Time:           2019/8/26
+ *   ProjectName:    XuSeekBar
+ *   ClassName:
+ *   Info:
+ */
+abstract class BaseActivity :AppCompatActivity() {
+}
