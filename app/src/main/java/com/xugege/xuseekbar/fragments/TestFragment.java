007e1dbd65ae46c84aa26a4fdb6d1e58d48957d6
kaixugege <604312074@qq.com>
2019-08-28 18:17:22 +0800

    fuck

---
+++ app/src/main/java/com/xugege/xuseekbar/fragments/TestFragment.java
@@ -0,0 +1,18 @@
+package com.xugege.xuseekbar.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+/**
+ * @Author: KaixuGege
+ * Time:           2019/8/28
+ * ProjectName:    XuSeekBar
+ * ClassName:
+ * Info:
+ */
+public class TestFragment extends Fragment {
+    public TestFragment(){
+        getArguments().putBundle("",new Bundle());
+    }
+
+}
