2863a925dfc3413bce06bfed1d04c09574daf8ea
kaixugege <604312074@qq.com>
2019-09-04 16:43:21 +0800

    fuck

--- app/src/main/java/com/xugege/xuseekbar/fragments/ContinuousFragment.kt
+++ app/src/main/java/com/xugege/xuseekbar/fragments/ContinuousFragment.kt
@@ -1,6 +1,6 @@
 package com.xugege.xuseekbar.fragments
 
-import android.annotation.SuppressLint
+import android.os.Bundle
 
 /**
  *   @Author:       KaixuGege
@@ -14,17 +14,26 @@ class ContinuousFragment() : BaseFragment() {
     private var username: String = ""
     private var age: Int = 0
 
-
     /**
      * 静态内部类
      */
     companion object {
         //apply像一个匿名函数，返回对象的本身
-        val newInstance = ContinuousFragment().apply {
-
+        fun newInstance(param1: String, title: String) = ContinuousFragment().apply {
+            arguments = Bundle().apply {
+                putString(PARAM1, param1)
+                putString(TITLE, title)
+            }
         }
+    }
 
+    fun getPageTitle(): String {
+        return arguments!!.get(TITLE).toString()
     }
 
 
 }
+
+private const val PARAM1 = "param1"
+private const val PARAM2 = "param2"
+private const val TITLE = "param1"
